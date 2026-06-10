12448bd34068a630c6df64fed6f3edc6134c053b
hackpython368 <prakashvidya368@gmail.com>
2026-06-10 08:52:06 +0530

    feature/ add filteration in doctor profile !

--- backend/src/doctor/doctor.controller.ts
+++ backend/src/doctor/doctor.controller.ts
@@ -1,40 +1,72 @@
-import { Controller, Get, UseGuards, Request, Patch, Body, Req, Post } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, Patch, Body, Req, Post, Query, Param } from '@nestjs/common';
 import { Roles } from 'src/auth/decorators/roles.decorators';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-guards';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { DoctorService } from './doctor.service';
 import { doctorData } from './Dto/doctor.dto';
+import { DoctorFilterDto } from './Dto/doctor-filter.dto';
 
 @Controller('doctor')
 export class DoctorController {
-  constructor(private readonly DoctorServices : DoctorService){}
+  constructor(private readonly DoctorServices : DoctorService,){}
 
-  @Get("")
-  listDoctor() {
-    return this.DoctorServices.listDoctor()
-  }
+@Get()
+getDoctorsBySpecialization(
+  @Query('specialization')
+  specialization?: string,
+  @Query('search')
+  search?: string,
+) {
+  if (specialization) {
+  return this.DoctorServices.getDoctors(
+    specialization,
+  );
+}
+if (search) {
+  return this.DoctorServices.getDoctorsByName(
+    search,
+  );
+}else{
+  return this.DoctorServices.listDoctor()
+}
+}
 
 
-    @Get('profile')
-
-@UseGuards(
-  JwtAuthGuard,
-  RolesGuard,
-)
+// @Get()
+//   listDoctor() {
+//     console.log("list doctor called")
+//     return this.DoctorServices.listDoctor()
+//   }
+  
+  
+  @Get('profile')
+  
+  @UseGuards(
+    JwtAuthGuard,
+    RolesGuard,
+  )
 
 @Roles('doctor')
 
 profile(
   @Request() req,
 ) {
-
+  
   return {
     message:
-      'Doctor Profile',
-
+    'Doctor Profile',
+    
     user:req.user
   };
 }
+
+@Get(':id')
+getDoctorById(
+  @Param('id') id: number,
+) {
+  return this.DoctorServices.getDoctorById(id);
+}
+
 
 @Post('profile')
 @UseGuards(
